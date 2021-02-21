daf48c63bf7236087c47981bbaed45f5b890d6fb
LadyNick <nnikki1013@gmail.com>
2021-02-21 12:46:51 -0800

    edits

--- source/main.c
+++ source/main.c
@@ -76,8 +76,8 @@ int CombineTick(int Combine_State){
 	unsigned char output;
 
 	switch(Combine_State){
-		case combine: ouput = keypad | (button << 6); break;
-		default: state = combine; break;
+		case combine: output = keypad | (button << 7); break;
+		default: Combine_State = combine; break;
 	}
 	PORTB = output;
 	return Combine_State;
@@ -90,8 +90,8 @@ int main(void) {
 	DDRB = 0XFF; PORTB = 0X00;
 	DDRC = 0XF0; PORTC = 0X0F;
 
-	static _task task1, task2, task3;
-	_task *tasks[] = {&task1, &task2, &task3};
+	static task task1, task2, task3;
+	task *tasks[] = {&task1, &task2, &task3};
 	const unsigned short numTasks = sizeof(tasks)/sizeof(task*);
 
 	const char start = -1;
@@ -99,20 +99,20 @@ int main(void) {
 	// TASK1: Keypad buttons
 	task1.state = start;
 	task1.period = 50;
-	task1.elapsedTIme = task1.period;
-	task1.TIckFct = &KeypadTick;
+	task1.elapsedTime = task1.period;
+	task1.TickFct = &KeypadTick;
 
 	//TASK2: Button press
 	task2.state = start;
         task2.period = 50;
-        task2.elapsedTIme = task2.period;
-        task2.TIckFct = &ButtonPressTick;
+        task2.elapsedTime = task2.period;
+        task2.TickFct = &ButtonPressTick;
 
 	//TASK3: Combine
 	task3.state = start;
         task3.period = 50;
-        task3.elapsedTIme = task3.period;
-        task3.TIckFct = &CombineTick;
+        task3.elapsedTime = task3.period;
+        task3.TickFct = &CombineTick;
 
 	unsigned long GCD = tasks[0]->period;
 	for(unsigned i=1; i<numTasks; i++) {
@@ -125,7 +125,7 @@ int main(void) {
 	unsigned short i;
         while(1){	
 		for(i=0; i<numTasks; i++){ //Scheduler code
-			if(tasks[i]->elapsedTIme == tasks[i]->period){
+			if(tasks[i]->elapsedTime == tasks[i]->period){
 				tasks[i]->state = tasks[i]->TickFct(tasks[i]->state);
 				tasks[i]->elapsedTime = 0;
 			}
