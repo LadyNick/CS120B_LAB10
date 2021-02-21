ca4a55e82f2697724fde9c4af2c6c0036f3bbf73
LadyNick <nnikki1013@gmail.com>
2021-02-21 10:53:12 -0800

    keypad2

--- source/main.c
+++ source/main.c
@@ -23,7 +23,24 @@ int main(void) {
     while (1) {
 	    x = GetKeypadKey();
 	    switch(x){
-		    case '\0': PORTB = 0X1F; break;
+		    case '\0': PORTB = 0X1F; break;		    	    
+			    case '1': PORTB = 0X01; break;
+		    	    case '2': PORT1B= 0X02; break;
+			    case '3': PORTB = 0X03; break;
+			    case '4': PORTB = 0X04; break;
+			    case '5': PORTB = 0X05; break;
+			    case '6': PORTB = 0X06; break;
+			    case '7': PORTB = 0X07; break;
+			    case '8': PORTB = 0X08; break;
+			    case '9': PORTB = 0X09; break;
+			    case 'A': PORTB = 0X0A; break;
+			    case 'B': PORTB = 0X0B; break;
+			    case 'C': PORTB = 0X0C; break;
+			    case 'D': PORTB = 0X0D; break;
+		            case '*': PORTB = 0X0E; break;      
+			    case '0': PORTB = 0X00; break;
+			    case '#': PORTB = 0X0F; break;
+			    default: PORTB = 0X1B; break; //should never occur
 
 	    }
 
