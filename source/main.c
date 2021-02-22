0040e10291906af7c84bebc967ee7638eebb422d
LadyNick <nnikki1013@gmail.com>
2021-02-21 22:21:26 -0800

    bell

--- source/main.c
+++ source/main.c
@@ -21,10 +21,11 @@ unsigned char y = 0;
 unsigned char keypad;
 unsigned char unlocked;
 unsigned char locked = 1;
-unsigned char count = 0;
+unsigned char A7;
 
 enum Keypad_States{input,release}Keypad_State;
 int KeypadTick(int Keypad_State){
+	unsigned char count;
 	switch(Keypad_State){
 	
 	case input:
@@ -104,6 +105,47 @@ int LockTick(int Lock_State){
 	return Lock_State;
 }
 
+enum Bell_States{press, melody}Bell_State;
+int BellTick(int Bell_State){
+	unsigned char count;
+
+	switch(Bell_State){
+		case press:
+			PWM_off();
+			count = 0;
+			if(A7){
+				Bell_State = melody;
+			}
+			else{
+				Bell_State = press;
+			}
+			break;
+		case melody:
+			PWM_on();
+			if((count == 0) || (count == 2)){
+				++count;
+				set_PWM(392.00);
+			}
+			if((count ==1) || (count == 3)){
+				++count;
+				set_PWM(440.00);
+			}
+			if((count == 4) || (count == 5)){
+				++count;
+				set_PWM(523.25);
+			}
+			if(!A7 && (count > 5)){
+				Bell_State = press;
+			}
+			else{
+			Bell_State = melody;
+			}
+			break;
+		default: Bell_State = press; break;
+	}
+	return Bell_State;
+}
+
 
 enum Combine_States{combine}Combine_State;
 int CombineTick(int Combine_State){
@@ -128,9 +170,10 @@ int CombineTick(int Combine_State){
 int main(void) {
     	DDRB = 0x7F; PORTB = 0X80;
 	DDRC = 0XF0; PORTC = 0X0F;
+	DDRA = 0X00; PINA = 0XFF;
 
-	static task task1, task2, task3;
-	task *tasks[] = {&task1, &task2, &task3};
+	static task task1, task2, task3, task4;
+	task *tasks[] = {&task1, &task2, &task3, &task4};
 	const unsigned short numTasks = sizeof(tasks)/sizeof(task*);
 
 	const char start = -1;
@@ -147,11 +190,17 @@ int main(void) {
         task2.elapsedTime = task2.period;
         task2.TickFct = &LockTick;
 
-	//TASK3: Combine
+	//TASK3: Doorbell
 	task3.state = start;
-        task3.period = 50;
-        task3.elapsedTime = task3.period;
-        task3.TickFct = &CombineTick;
+	task3.period = 500;
+	task3.elapsedTime = task3.period;
+	task3.TickFct = &BellTick;
+
+	//TASK4: Combine
+	task4.state = start;
+        task4.period = 50;
+        task4.elapsedTime = task3.period;
+        task4.TickFct = &CombineTick;
 
 	unsigned long GCD = tasks[0]->period;
 	for(unsigned i=1; i<numTasks; i++) {
@@ -163,6 +212,7 @@ int main(void) {
 
 	unsigned short i;
         while(1){	
+		A7 = ~PINA >> 7;
 		for(i=0; i<numTasks; i++){ //Scheduler code
 			if(tasks[i]->elapsedTime == tasks[i]->period){
 				tasks[i]->state = tasks[i]->TickFct(tasks[i]->state);
@@ -175,4 +225,36 @@ int main(void) {
 	}
 	return 0;
 
+}
+
+void set_PWM(double frequency){
+	static double current_frequency;
+
+	if(frequency != current_frequency){
+		if(!frequency) {TCCR3B &= 0X08;}
+		else { TCCR3B |= 0x03; }
+
+		if(frequency < 0.954) { OCR3A = 0XFFFF; }
+
+		else if(frequency > 31250) { OCR3A = 0X0000; }
+
+		else { OCR3A = (short) (8000000 / (128*frequency)) - 1; }
+
+		TCNT3 = 0;
+		current_frequency = frequency;
+
+	}
+}
+
+void PWM_on(){
+	TCCR3A = (1 << COM3A0);
+
+	TCCR3B = (1 << WGM32) | (1 << CS31) | (1 << CS30);
+
+	set_PWM(0);
+}
+
+void PWM_off(){
+	TCCR3A = 0X00;
+	TCCR3B = 0X00;
 }
