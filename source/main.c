13e9ef349d3741c102fe427de3c967ffd06ea641
LadyNick <nnikki1013@gmail.com>
2021-02-22 19:19:15 -0800

    starting part 3 over

--- source/main.c
+++ source/main.c
@@ -16,17 +16,16 @@
 #include "timer.h"
 #endif
 
-unsigned char A7;
 unsigned char x;
 unsigned char y = 0;
 unsigned char keypad = 0;
 unsigned char unlocked = 0;
-//unsigned char locked = 1;
+unsigned char count;
+
 unsigned char bell = 0;
 unsigned char tuneperiod = 200;
-unsigned long counter;
-unsigned char count;
-unsigned char toggle = 0;
+unsigned char A7;
+
 
 enum Keypad_States{input,release}Keypad_State;
 int KeypadTick(int Keypad_State){
@@ -114,64 +113,16 @@ int BellTick(int Bell_State){
 
 	switch(Bell_State){
 		case wait_press:
-	/*		toggle = 0;
-		//	bell = 0;
-		//	counter = 0;
-		//	tuneperiod = 200; //its checking buttonpress every 200 m
 			if(A7){
 				Bell_State = melody;
-				tuneperiod = 3;
 			}
 			else{
 				Bell_State = wait_press;
 			}
-		//	Bell_State = wait_press;
-			break;
-		case melody:
-			if(toggle == 0){
-				toggle = 1;
-				bell = 0;
-			}
-			else if(toggle == 1){
-				toggle = 0;
-				bell = 0x40;
-			}
-			Bell_State = melody;
-			if( (counter >= 0) && (counter < 5000) ){
-				tuneperiod = 3; 	
-				counter += tuneperiod; //at this point tuneperiod is 3, meaning this will be ticking every 3ms to get a certain sound 
-				//instead of counter increasing once per 3ms, were increasing it 3 times per 3 ms, so that its still counting normally
-				//this goes up to 498 because it doesnt divide evenly but its about half a second
-				//it will leave this if when its done as 501
-			}
-			if( (counter >= 500) && (counter < 1000)){
-				tuneperiod = 5;
-				counter += tuneperiod;
-				//this will go up to 996 because 499/5 -> 99 because it gies up to 495 max, then 501 + 495 is 996
-				//it will leave as 1001
-			} 
-
-			//this will be how it stops
-			
-			if((!(~PINA >> 7)) && (counter > 5000)){
-				bell = 0;
-				toggle = 0;
-				Bell_State = wait_press;
-			}
-			else{
-				Bell_State = melody;
-			} */
-		/*	++counter;
-			if(counter > 5000){
-				Bell_State = wait_press;
-				bell = 0;
-				toggle = 0;
-				tuneperiod = 200;
-			}
-			else{
-				Bell_State = melody;
-			}*/
 			break; 
+		case melody:
+			bell = 0x02; 
+			break;
 		default: Bell_State = wait_press; break;
 	}
 	return Bell_State;
@@ -180,11 +131,11 @@ int BellTick(int Bell_State){
 
 enum Combine_States{combine}Combine_State;
 int CombineTick(int Combine_State){
-	unsigned char output;
 
 	switch(Combine_State){
 		case combine:
-			PORTB = unlocked;
+			PORTB = unlocked + bell;
+			Combine_State = combine;
 			break;
 		default: Combine_State = combine; break;
 	}
@@ -217,27 +168,22 @@ task2.TickFct = &LockTick;
 
 //TASK3: Doorbell
 task3.state = start;
-task3.period = 2; //we have to start very low because the freq are all low
+task3.period = 200; //because button check 200ms so this is the base
 task3.elapsedTime = task3.period;
 task3.TickFct = &BellTick;
 
 //TASK4: Combine
 task4.state = start;
 task4.period = 50;
-task4.elapsedTime = task3.period;
+task4.elapsedTime = task4.period;
 task4.TickFct = &CombineTick;
 
-unsigned long GCD = tasks[0]->period;
-	for(unsigned i=1; i<numTasks; i++) {
-		GCD = findGCD(GCD,tasks[i]->period);
-	}
-
-	TimerSet(1); //This will end up being set to 1
+	TimerSet(1);
 	TimerOn();
 
 	unsigned short i;
         while(1){
-		A7 = PINA >> 7;	
+		A7 = ~PINA & 0x80;	
 		task3.period = tuneperiod;
 
 		for(i=0; i<numTasks; i++){ //Scheduler code
